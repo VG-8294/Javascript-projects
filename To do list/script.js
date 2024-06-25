9ec440e5078fde693f4a8105723cb07e5a2c148b
Vishal <vishalgautam.haridwar@gmail.com>
2024-06-25 13:31:31 +0530

    Some changes

--- To do list/script.js
+++ To do list/script.js
@@ -19,25 +19,38 @@ add.addEventListener('click', function () {
     if (count === 1) {
         createResult(); //Creating the result only one time at the start
     }
+    else {
+
+    }
+
     const taskValue = input.value.trim();
     if (taskValue) { // checks whether the input is empty or not 
         tasks.push(taskValue);
+        // Creating a container for each task and its horizontal line
+        const taskContainer = document.createElement('div');
+        taskContainer.classList.add('task-container');
+        results.appendChild(taskContainer);
 
-        //Creating a div indside the result section
-        let listContent = document.createElement('div')
-        listContent.id = "listContent"
-        results.appendChild(listContent)
+        // Creating a div inside the task container
+        let listContent = document.createElement('div');
+        listContent.id = "listContent";
+        taskContainer.appendChild(listContent);
 
         //creating a list item for adding task into it
         const listItem = document.createElement('li');
         listItem.textContent = taskValue;
-        
+
+        // Creating a horizontal line for the task
+        const horiz = document.createElement('hr');
+        taskContainer.appendChild(horiz);
+
         //Creating one more div inside listContent for list items
         let listContentInn = document.createElement('div')
         listContentInn.id = "listContent-inn"
         listContent.appendChild(listContentInn)
         //Creating a checkbox
         var x = document.createElement("Input");
+        x.id = "checkbox"
         x.setAttribute("type", "checkbox");
         listContentInn.appendChild(x);
         x.addEventListener('click', function () {
@@ -63,19 +76,14 @@ add.addEventListener('click', function () {
         del.id = "delete"
         del.textContent = 'Delete'
         buttons.appendChild(del)
-        //Creating a horizontal line after each task
-        const horiz = document.createElement('hr')
-        results.appendChild(horiz)
+
 
         //Removing the value of input field after addition of task
-        input.value = ""; 
-        //Functionality of Delete and Edit buttons
-        //Delete
+        input.value = "";
+        //Functionality of Delete and edit buttons
         del.addEventListener('click', function () {
-            listContent.remove();
-            horiz.remove()
+            taskContainer.remove()
         })
-        //Edit
         edit.addEventListener('click', function () {
             let editInput = document.createElement('input');
             editInput.id = "InputEdit"
@@ -100,7 +108,6 @@ add.addEventListener('click', function () {
                 saveChanges();
             });
         });
-
 
     }
 });
