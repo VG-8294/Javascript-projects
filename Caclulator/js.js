061dd5d12fa4c259d416ebdd35c3905fdd584ff3
Vishal <vishalgautam.haridwar@gmail.com>
2024-07-01 12:30:02 +0530

    readme file created

--- Caclulator/js.js
+++ Caclulator/js.js
@@ -1,4 +1,36 @@
 const calc = document.querySelectorAll('.calculator')
-document.getElementById('inputBox', function(){
+const ops = document.querySelectorAll('.operators')
+const dot = document.querySelector("#dot")
+const AC = document.querySelector('#AC')
+const del = document.querySelector('#del')
+const Input = document.querySelector('#inputBox')
+const numBtns  = document.querySelectorAll("#num")
 
+ops.forEach(op => {
+   op.addEventListener('click', function(){
+      Input.value += op.textContent
+   });
+});
+dot.addEventListener('click', function(){
+   if(Input.value.includes(".")){
+      return;
+   } 
+   else{
+      Input.value += dot.textContent 
+   }
+})
+
+AC.addEventListener('click', function(){
+   Input.value = "";
+})
+
+del.addEventListener('click', function(){
+   Input.value = Input.value.slice(0, -1);
+})
+
+
+numBtns.forEach(numBtn => {
+   numBtn.addEventListener('click', function(){
+      Input.value += parseInt(numBtn.textContent)
+   })
 })
---
+++ To do list/readme.md
@@ -0,0 +1,13 @@
+![alt text](<Screenshot 2024-07-01 122542-1.png>)
+
+To-Do List
+The To-Do List is a dynamic and interactive feature designed to help you keep track of tasks efficiently. It offers a variety of functionalities that make task management seamless and straightforward.
+
+Features:
+Add Tasks: Easily add new tasks to your list with a simple input form. Each task can have a title and optional description.
+
+Edit Tasks: Modify existing tasks to update their information. This allows you to make changes to the task's title or description as needed.
+
+Delete Tasks: Remove tasks that are no longer needed from your list. This helps in keeping your task list clean and relevant.
+
+Mark Tasks as Completed: Once a task is done, you can cross it off your list by marking it as completed. This visually distinguishes completed tasks from those that are still pending.
